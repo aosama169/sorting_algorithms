1703fce4b65fba1a1b31d8bace8924b81da60426
Aosama <ahmed.osama@elsewedyintel.com>
2024-02-21 00:06:52 +0200

    sort

--- sort.h
+++ sort.h
@@ -26,6 +26,6 @@ void shell_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-
+void merge_sort(int *array, size_t size);
 
 #endif
